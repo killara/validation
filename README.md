d3e4df4ed0bbff2af06ccda334c41711173765ae
Runrioter <runrioter@qq.com>
2018-04-22 02:53:44 +0800

    feat: Implement RULE: alpha regexp required

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 [![npm](https://img.shields.io/npm/v/@killara/validation.svg)](https://www.npmjs.com/package/@killara/validation)
 [![Travis branch](https://img.shields.io/travis/killara/validation/master.svg)](https://travis-ci.org/killara/validation)
 [![Codecov branch](https://img.shields.io/codecov/c/github/killara/validation/master.svg)](https://codecov.io/github/killara/validation?branch=master)
+[![npm download](https://img.shields.io/npm/dt/@killara/validation.svg)](https://www.npmjs.com/package/@killara/validation)
 
 [**WIP**] A validator for HTTP request parameters
 
@@ -33,5 +34,25 @@ const rules = {
   }
 }
 
-const errors = validation.validate(values, rules);
+const errors = await validation.validate(values, rules);
 ```
+
+## Rule
+
+* required
+  * options (default: false)
+    * type: `boolean`
+    * example
+      * `required: true`
+* alpha
+  * options (default: may be any length)
+    * type: `object`
+    * properties
+      * len
+    * example
+      * `alpha: { len: 6 }`
+* regexp
+  * options (no default: an object option is a must)
+    * type: `regexp`
+    * example
+      * `regexp: /^[0-9a-zA-z]{8:16}$/`
--- test/index.test.js
+++ test/index.test.js
@@ -3,18 +3,95 @@
 const validation = require('..');
 
 describe('validation', () => {
-  describe('rules', () => {
-    test('required', () => {
-      const values = {
-        password: 'admin',
-      };
-      const rules = {
-        username: {
-          required: true,
-        },
-      };
-      const expected = [{ code: 'missing_field', field: 'username', message: 'The field is a must' }];
-      expect(validation.validate(values, rules)).resolves.toEqual(expected);
+  describe('object rules', () => {
+    describe('required', () => {
+      test('true', () => {
+        expect.assertions(1);
+        const values = {
+          password: 'admin',
+        };
+        const rules = {
+          username: {
+            required: true,
+          },
+        };
+        const expected = [{ code: 'missing_field', field: 'username', message: 'The field is a must' }];
+        return expect(validation.validate(values, rules)).resolves.toEqual(expected);
+      });
+      test('false', () => {
+        expect.assertions(1);
+        const values = {
+          password: 'admin',
+        };
+        const rules = {
+          username: {
+            required: false,
+          },
+        };
+        return expect(validation.validate(values, rules)).resolves.toBeUndefined();
+      });
+    });
+
+    describe('alpha', () => {
+      test('default', () => {
+        expect.assertions(1);
+        const values = {
+          username: 'admin',
+        };
+        const rules = {
+          username: {
+            required: true,
+            alpha: {},
+          },
+        };
+        return expect(validation.validate(values, rules)).resolves.toBeUndefined();
+      });
+      test('{ len: 6 }', () => {
+        expect.assertions(1);
+        const values = {
+          username: 'admin',
+        };
+        const rules = {
+          username: {
+            required: true,
+            alpha: { len: 6 },
+          },
+        };
+        const messages = {
+          'username.alpha': 'The field only contains ${len} letters.',
+        };
+        const expected = [{ code: 'invalid', field: 'username', message: 'The field only contains 6 letters.' }];
+        return expect(validation.validate(values, rules, messages)).resolves.toEqual(expected);
+      });
+    });
+
+    describe('regexp', () => {
+      test('no regexp should throw an exception', async () => {
+        expect.assertions(1);
+        const values = {
+          username: 'admin',
+        };
+        const rules = {
+          username: {
+            required: true,
+            regexp: {},
+          },
+        };
+        await expect(validation.validate(values, rules)).rejects.toThrowError('Options for regexp rule should be a regular expression');
+      });
+      test('/^123456$/', async () => {
+        expect.assertions(1);
+        const values = {
+          password: '123456',
+        };
+        const rules = {
+          password: {
+            required: true,
+            regexp: /^123456$/,
+          },
+        };
+        await expect(validation.validate(values, rules)).resolves.toBeUndefined();
+      });
     });
   });
 });
